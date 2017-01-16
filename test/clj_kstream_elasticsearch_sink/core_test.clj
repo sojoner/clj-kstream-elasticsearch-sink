8041de199fcfe1a0badfa08c092eaf8acc84ecf2
Sojoner <hagentoennies@googlemail.com>
2017-01-16 11:21:10 +0100

    Initial commit

---
+++ test/clj_kstream_elasticsearch_sink/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-kstream-elasticsearch-sink.core-test
+  (:require [clojure.test :refer :all]
+            [clj-kstream-elasticsearch-sink.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
